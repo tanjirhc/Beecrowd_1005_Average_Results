95ab3a022924a786b4dc0165b44aa2275c263a13
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-08 11:19:37 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beecrowd_1005_Average_Results
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B;
+            A = Convert.ToDouble(Console.ReadLine());
+            B = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("MEDIA = " + ((A*3.5 + B*7.5)/(3.5+7.5)).ToString("0.00000"));
+            Console.ReadKey();
+        }
+    }
+}
